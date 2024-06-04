d7cb383eacf164b01fc6ed2583cbb67f982253d4
Brandon Valenzuela <brandonvalenzuela48@gmail.com>
2024-06-04 10:19:19 -0700

    first commit

---
+++ Api/Hubs/ChatHub.cs
@@ -0,0 +1,96 @@
+﻿using Api.Dtos;
+using Api.Services;
+using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Threading.Tasks;
+
+namespace Api.Hubs
+{
+    public class ChatHub : Hub
+    {
+        private readonly ChatService _chatService;
+
+        public ChatHub(ChatService chatService)
+        {
+            _chatService = chatService;
+        }
+
+
+        public override async Task OnConnectedAsync()
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, "Chat1");
+            await Clients.Caller.SendAsync("UserConnected");
+        }
+
+
+        public override async Task OnDisconnectedAsync(Exception exception)
+        {
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, "Chat1");
+            var user = _chatService.GetUserByConnectionId(Context.ConnectionId);
+            _chatService.RemoveUserFromList(user);
+
+            await DisplayOnlineUsers();
+
+            await base.OnDisconnectedAsync(exception);
+        }
+
+
+        public async Task AddUserConnectionId(string name)
+        {
+            _chatService.AddUserConnectionId(name, Context.ConnectionId);
+            await DisplayOnlineUsers();
+        }
+
+
+        public async Task ReceiveMessage(MessageDto message)
+        {
+            await Clients.Group("Chat1").SendAsync("NewMessage", message);
+        }
+
+
+        public async Task CreatePrivateChat(MessageDto message)
+        {
+            string privateGroupName = GetPrivateGroupName(message.From, message.To);
+            await Groups.AddToGroupAsync(Context.ConnectionId, privateGroupName);
+            var toConnectionId = _chatService.GetConnectionIdByUser(message.To);
+            await Groups.AddToGroupAsync(toConnectionId, privateGroupName);
+
+            // opening private chatbox for the other end  user
+            await Clients.Client(toConnectionId).SendAsync("OpenPrivateChat", message);
+        }
+
+
+        public async Task RecivePrivateMessage(MessageDto message)
+        {
+            string privateGroupName = GetPrivateGroupName(message.From, message.To);
+            await Clients.Group(privateGroupName).SendAsync("NewPrivateMessage", message);
+        }
+
+
+        public async Task RemovePrivateChat(string from, string to)
+        {
+            string privateGroupName = GetPrivateGroupName(from, to);
+            await Clients.Group(privateGroupName).SendAsync("ClosePrivateChat");
+
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, privateGroupName);
+            var toConnectionId = _chatService.GetConnectionIdByUser(to);
+            await Groups.RemoveFromGroupAsync(toConnectionId, privateGroupName);
+        }
+
+
+        //Private Methods
+
+        private async Task DisplayOnlineUsers()
+        {
+            var onlineUsers = _chatService.GetOnlineUsers();
+            await Clients.Group("Chat1").SendAsync("OnlineUsers", onlineUsers);
+        }
+
+        private string GetPrivateGroupName(string from, string to)
+        {
+            // from: john, to: david  return "david-john"
+            var stringCompare = string.CompareOrdinal(from, to) < 0;
+            return stringCompare ? $"{from}-{to}" : $"{to}-{from}";
+        }
+    }
+}
